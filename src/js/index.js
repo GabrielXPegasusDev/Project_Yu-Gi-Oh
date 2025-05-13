77da827f34255dcdc08d5e289c92778c5a3b5b06
GabrielXPegasus <gabrielxpegasus@gmail.com>
2025-05-13 18:26:24 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,13 @@ const btnBackward = document.getElementById("btn-backward");
 const cards = document.querySelectorAll(".card");
 let currentCard = 0;
 
+cards.forEach((card) => {
+  card.addEventListener("click", function () {
+    toggleCardFlip(card);
+    toggleDescriptionVisibility(card);
+  });
+});
+
 btnForward.addEventListener("click", function () {
   if (currentCard === cards.length - 1) return;
 
@@ -20,6 +27,17 @@ btnBackward.addEventListener("click", function () {
   currentCard--;
   showCard(currentCard);
 });
+
+function toggleDescriptionVisibility(card) {
+  const description = card.querySelector(".description");
+  description.classList.toggle("hide");
+}
+
+function toggleCardFlip(card) {
+  const flippedCard = card.querySelector(".flipped-card");
+  card.classList.toggle("flip");
+  flippedCard.classList.toggle("show-flipped-card");
+}
 
 function showCard(currentCard) {
   cards[currentCard].classList.add("selected");
